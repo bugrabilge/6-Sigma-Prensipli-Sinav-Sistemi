6b24be730b023034bb60530daf60bc7260a6d7c1
Buğra Bilge Çelik <79867464+bugrabilge@users.noreply.github.com>
2022-03-21 23:42:02 +0300

    hata düzeltme

---
+++ 6-Sigma-Prensipli-Sinav-Sistemi/SSorumluForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_Sigma_Prensipli_Sinav_Sistemi
+{
+    public partial class SSorumluForm : Form
+    {
+        public SSorumluForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
