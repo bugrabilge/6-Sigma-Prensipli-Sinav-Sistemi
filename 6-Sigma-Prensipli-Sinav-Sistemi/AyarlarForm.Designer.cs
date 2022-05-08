a6f7420146b322f9e2d1603fb8e8dd98786082f1
Buğra Bilge Çelik <79867464+bugrabilge@users.noreply.github.com>
2022-05-08 12:39:18 +0300

    tasarım ve hata giderme

--- 6-Sigma-Prensipli-Sinav-Sistemi/AyarlarForm.Designer.cs
+++ 6-Sigma-Prensipli-Sinav-Sistemi/AyarlarForm.Designer.cs
@@ -29,6 +29,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AyarlarForm));
             this.txtIlk = new System.Windows.Forms.TextBox();
             this.txtIkinci = new System.Windows.Forms.TextBox();
             this.txtUcuncu = new System.Windows.Forms.TextBox();
@@ -37,7 +38,6 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             this.txtAltinci = new System.Windows.Forms.TextBox();
             this.btnOnayla = new System.Windows.Forms.Button();
             this.lblAciklama = new System.Windows.Forms.Label();
-            this.lblAciklama2 = new System.Windows.Forms.Label();
             this.lblIlk = new System.Windows.Forms.Label();
             this.lblIki = new System.Windows.Forms.Label();
             this.lblUc = new System.Windows.Forms.Label();
@@ -48,78 +48,73 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // 
             // txtIlk
             // 
-            this.txtIlk.Location = new System.Drawing.Point(95, 76);
+            this.txtIlk.Location = new System.Drawing.Point(95, 90);
             this.txtIlk.Name = "txtIlk";
             this.txtIlk.Size = new System.Drawing.Size(100, 20);
             this.txtIlk.TabIndex = 0;
             // 
             // txtIkinci
             // 
-            this.txtIkinci.Location = new System.Drawing.Point(95, 102);
+            this.txtIkinci.Location = new System.Drawing.Point(95, 116);
             this.txtIkinci.Name = "txtIkinci";
             this.txtIkinci.Size = new System.Drawing.Size(100, 20);
             this.txtIkinci.TabIndex = 1;
             // 
             // txtUcuncu
             // 
-            this.txtUcuncu.Location = new System.Drawing.Point(95, 128);
+            this.txtUcuncu.Location = new System.Drawing.Point(95, 142);
             this.txtUcuncu.Name = "txtUcuncu";
             this.txtUcuncu.Size = new System.Drawing.Size(100, 20);
             this.txtUcuncu.TabIndex = 2;
             // 
             // txtDorduncu
             // 
-            this.txtDorduncu.Location = new System.Drawing.Point(95, 154);
+            this.txtDorduncu.Location = new System.Drawing.Point(95, 168);
             this.txtDorduncu.Name = "txtDorduncu";
             this.txtDorduncu.Size = new System.Drawing.Size(100, 20);
             this.txtDorduncu.TabIndex = 3;
             // 
             // txtBesinci
             // 
-            this.txtBesinci.Location = new System.Drawing.Point(95, 180);
+            this.txtBesinci.Location = new System.Drawing.Point(95, 194);
             this.txtBesinci.Name = "txtBesinci";
             this.txtBesinci.Size = new System.Drawing.Size(100, 20);
             this.txtBesinci.TabIndex = 4;
             // 
             // txtAltinci
             // 
-            this.txtAltinci.Location = new System.Drawing.Point(95, 206);
+            this.txtAltinci.Location = new System.Drawing.Point(95, 220);
             this.txtAltinci.Name = "txtAltinci";
             this.txtAltinci.Size = new System.Drawing.Size(100, 20);
             this.txtAltinci.TabIndex = 5;
             // 
             // btnOnayla
             // 
+            this.btnOnayla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.btnOnayla.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOnayla.ForeColor = System.Drawing.Color.White;
             this.btnOnayla.Location = new System.Drawing.Point(107, 249);
             this.btnOnayla.Name = "btnOnayla";
-            this.btnOnayla.Size = new System.Drawing.Size(75, 23);
+            this.btnOnayla.Size = new System.Drawing.Size(75, 38);
             this.btnOnayla.TabIndex = 6;
             this.btnOnayla.Text = "Onayla";
-            this.btnOnayla.UseVisualStyleBackColor = true;
+            this.btnOnayla.UseVisualStyleBackColor = false;
             this.btnOnayla.Click += new System.EventHandler(this.btnOnayla_Click);
             // 
             // lblAciklama
             // 
-            this.lblAciklama.AutoSize = true;
-            this.lblAciklama.Location = new System.Drawing.Point(21, 38);
+            this.lblAciklama.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAciklama.Location = new System.Drawing.Point(3, 22);
             this.lblAciklama.Name = "lblAciklama";
-            this.lblAciklama.Size = new System.Drawing.Size(252, 13);
+            this.lblAciklama.Size = new System.Drawing.Size(280, 54);
             this.lblAciklama.TabIndex = 7;
-            this.lblAciklama.Text = "Sigma algoritmasında soru çıkma sıklığını değiştirmek";
-            // 
-            // lblAciklama2
-            // 
-            this.lblAciklama2.AutoSize = true;
-            this.lblAciklama2.Location = new System.Drawing.Point(32, 51);
-            this.lblAciklama2.Name = "lblAciklama2";
-            this.lblAciklama2.Size = new System.Drawing.Size(226, 13);
-            this.lblAciklama2.TabIndex = 8;
-            this.lblAciklama2.Text = "için kutuları doldurup Onayla butonuna basınız.";
+            this.lblAciklama.Text = "Sigma algoritmasında soru çıkma sıklığını değiştirmek için alanları doldurup Onay" +
+    "la butonuna basınız.";
             // 
             // lblIlk
             // 
             this.lblIlk.AutoSize = true;
-            this.lblIlk.Location = new System.Drawing.Point(63, 79);
+            this.lblIlk.Location = new System.Drawing.Point(63, 93);
             this.lblIlk.Name = "lblIlk";
             this.lblIlk.Size = new System.Drawing.Size(16, 13);
             this.lblIlk.TabIndex = 9;
@@ -128,7 +123,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // lblIki
             // 
             this.lblIki.AutoSize = true;
-            this.lblIki.Location = new System.Drawing.Point(63, 105);
+            this.lblIki.Location = new System.Drawing.Point(63, 119);
             this.lblIki.Name = "lblIki";
             this.lblIki.Size = new System.Drawing.Size(16, 13);
             this.lblIki.TabIndex = 10;
@@ -137,7 +132,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // lblUc
             // 
             this.lblUc.AutoSize = true;
-            this.lblUc.Location = new System.Drawing.Point(63, 131);
+            this.lblUc.Location = new System.Drawing.Point(63, 145);
             this.lblUc.Name = "lblUc";
             this.lblUc.Size = new System.Drawing.Size(16, 13);
             this.lblUc.TabIndex = 11;
@@ -146,7 +141,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // lblDort
             // 
             this.lblDort.AutoSize = true;
-            this.lblDort.Location = new System.Drawing.Point(63, 157);
+            this.lblDort.Location = new System.Drawing.Point(63, 171);
             this.lblDort.Name = "lblDort";
             this.lblDort.Size = new System.Drawing.Size(16, 13);
             this.lblDort.TabIndex = 12;
@@ -155,7 +150,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // lblBes
             // 
             this.lblBes.AutoSize = true;
-            this.lblBes.Location = new System.Drawing.Point(63, 183);
+            this.lblBes.Location = new System.Drawing.Point(63, 197);
             this.lblBes.Name = "lblBes";
             this.lblBes.Size = new System.Drawing.Size(16, 13);
             this.lblBes.TabIndex = 13;
@@ -164,7 +159,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // lblAlti
             // 
             this.lblAlti.AutoSize = true;
-            this.lblAlti.Location = new System.Drawing.Point(63, 209);
+            this.lblAlti.Location = new System.Drawing.Point(63, 223);
             this.lblAlti.Name = "lblAlti";
             this.lblAlti.Size = new System.Drawing.Size(16, 13);
             this.lblAlti.TabIndex = 14;
@@ -174,6 +169,7 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(285, 299);
             this.Controls.Add(this.lblAlti);
             this.Controls.Add(this.lblBes);
@@ -181,7 +177,6 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             this.Controls.Add(this.lblUc);
             this.Controls.Add(this.lblIki);
             this.Controls.Add(this.lblIlk);
-            this.Controls.Add(this.lblAciklama2);
             this.Controls.Add(this.lblAciklama);
             this.Controls.Add(this.btnOnayla);
             this.Controls.Add(this.txtAltinci);
@@ -190,8 +185,9 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
             this.Controls.Add(this.txtUcuncu);
             this.Controls.Add(this.txtIkinci);
             this.Controls.Add(this.txtIlk);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AyarlarForm";
-            this.Text = "AyarlarForm";
+            this.Text = "Ayarlar";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,7 +203,6 @@ namespace _6_Sigma_Prensipli_Sinav_Sistemi
         private System.Windows.Forms.TextBox txtAltinci;
         private System.Windows.Forms.Button btnOnayla;
         private System.Windows.Forms.Label lblAciklama;
-        private System.Windows.Forms.Label lblAciklama2;
         private System.Windows.Forms.Label lblIlk;
         private System.Windows.Forms.Label lblIki;
         private System.Windows.Forms.Label lblUc;
